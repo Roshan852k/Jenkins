cb40a6f5f2c5433baf6bfc4d345d67d74e5fde4c
Roshan852k <68069185+Roshan852k@users.noreply.github.com>
2025-01-27 22:39:42 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,2 @@
-#! /usr/bin/python3
-# This Phython program will print Hellow World...
-print("Hello World Demo......\n")
-print("Hello World Demo......\n")
-print("Hello World Demo......\n")
-print("Hello World Demo......\n")
-print("Hello World Demo......\n")
+print("Hello Python")
+
